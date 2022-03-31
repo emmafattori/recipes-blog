a59dacb77cca3678e727a657dad9123c0032a71d
Emma Fattori <emma@applydigital.co>
2022-03-31 10:34:53 -0400

    setting up contentful, and env variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
+import React from 'react';
 import './App.css';
+import { client } from './client';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    client.getEntries()
+      .then((res) => {
+        console.log(res)
+      })
+      .catch(console.error)
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <header>
+            <div className="wrapper">
+              <span>React and Contentful</span>
+            </div>
+          </header>
+          <main>
+            <div className="wrapper">
+
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
